3bcefdc92470acc2a706e35832ebaf3f8f20e22f
MANZI Mike <48416679+MikeManzi@users.noreply.github.com>
2024-07-05 18:16:49 +0200

    API advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 """ Get the titles of the first 10 hot posts for a given subreddit."""
+
+import json
 import requests
+import sys
 
 
 def top_ten(subreddit):
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-agent': 'myRedditScript/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            top_posts = [post['data']['title']
+                         for post in data['data']['children']]
+            for post_title in top_posts:
+                print(post_title)
+        else:
+            print("No posts found")
     else:
-        print(None)
+        print("None")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
