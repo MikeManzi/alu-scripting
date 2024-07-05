a44c7097c9117fe3f2f6a186375435c813a0f221
MANZI Mike <48416679+MikeManzi@users.noreply.github.com>
2024-07-05 18:29:59 +0200

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,8 +7,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Set a custom header user-agent """
+    headers = {"User-Agent": "ALU-scripting API 0.1"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
